c36d78b877dce404629fcb44757d99f6ec60d6b3
Gadjev <gadjevalexandar@gmail.com>
2017-10-08 14:31:42 +0200

    delete sample app and initialize pomodoro app

--- src/com/gadjev/pomodoro/PomodoroApp.java
+++ src/com/gadjev/pomodoro/PomodoroApp.java
@@ -1,4 +1,16 @@
 package com.gadjev.pomodoro;
 
-public class PomodoroApp {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PomodoroApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
