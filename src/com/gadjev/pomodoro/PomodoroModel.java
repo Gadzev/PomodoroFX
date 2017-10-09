6f8190b6d899571985da4cc267a9c2371a97d06f
Gadjev <gadjevalexandar@gmail.com>
2017-10-09 12:48:19 +0200

    Limit thread queue size to only one thread per time

--- src/com/gadjev/pomodoro/PomodoroModel.java
+++ src/com/gadjev/pomodoro/PomodoroModel.java
@@ -3,41 +3,50 @@ package com.gadjev.pomodoro;
 
 import javafx.application.Platform;
 import javafx.scene.control.Label;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import java.util.concurrent.*;
 
 public class PomodoroModel {
 
-    private ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private ExecutorService executorService = new ThreadPoolExecutor(
+            1, 1,
+            60, TimeUnit.SECONDS,
+            new SynchronousQueue<>());
 
     public void start(final Pomodoro pomodoro, final Label timeLabel, final Label messageLabel) {
+
         messageLabel.setText("");
-      executorService.execute(() -> {
-              int time = convertToSeconds(pomodoro.getMinutes());
-              long delay = time * 1000;
+    try {
+        executorService.execute(() -> {
+            int time = convertToSeconds(pomodoro.getMinutes());
+            long delay = time * 1000;
 
-              do {
-                  final int minutes = time / 60;
-                  final int seconds = time % 60;
-                  Platform.runLater(()  -> {
+            do {
+                final int minutes = time / 60;
+                final int seconds = time % 60;
+                Platform.runLater(() -> {
 
-                          timeLabel.setText("" + minutes + ":" + seconds);
+                    timeLabel.setText("" + minutes + ":" + seconds);
 
-                  });
-                  try {
-                      Thread.sleep(1000);
-                  } catch (InterruptedException e) {
-                      System.out.println("Interrupted: " + e);
-                      stop();
-                  }
-                  time -= 1;
-                  delay -= 1000;
-              } while (delay != 0);
-              Platform.runLater(() -> {
-                      messageLabel.setText("Time is up");
-                      pomodoro.ring();
-              });
-      });
+                });
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("Interrupted: " + e);
+                    stop();
+                }
+                time -= 1;
+                delay -= 1000;
+            } while (delay != 0);
+            Platform.runLater(() -> {
+                messageLabel.setText("Time is up");
+                pomodoro.ring();
+            });
+        });
+    } catch (RejectedExecutionException e) {
+        messageLabel.setText("Wait for current pomodoro to finish");
+    }
+
     }
 
     public void stop() {
